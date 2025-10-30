ed25cf2aed46e84eb4a1f409c7efba8aa05b895a
FilSantos12 <filipe.rodri.stos@gmail.com>
2025-10-29 22:16:05 -0300

    caminho correto para o GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@
 
   export default defineConfig({
     plugins: [react()],
-    base: 'https://github.com/filsantos12/ProMed',
+    base: '/ProMed/',
     resolve: {
       extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
       alias: {
