e628bfecda9575c1ab2c5bbe49a8b5d894334a50
FilSantos12 <filipe.rodri.stos@gmail.com>
2026-01-31 17:04:39 -0300

    Corrigir case sensitivity no import do Toast

--- src/contexts/ToastContext.tsx
+++ src/contexts/ToastContext.tsx
@@ -1,6 +1,6 @@
 import React, { createContext, useContext, useState, useCallback } from 'react';
-import ToastContainer from '../components/Toast/ToastContainer';
-import { ToastProps, ToastType } from '../components/Toast/Toast';
+import ToastContainer from '../components/toast/ToastContainer';
+import { ToastProps, ToastType } from '../components/toast/Toast';
 
 interface ToastContextData {
   showToast: (type: ToastType, message: string, duration?: number) => void;
